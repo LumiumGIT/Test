cb45e2a842e2d172478a9cf14847987907c32eb1
LumiumGIT <sintra@lumium.tech>
2026-02-06 00:03:34 +0100

    Delete Lumium directory

--- Lumium/Context/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Lumium.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Lumium.Context;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<Client> Clients { get; set; }
-}
