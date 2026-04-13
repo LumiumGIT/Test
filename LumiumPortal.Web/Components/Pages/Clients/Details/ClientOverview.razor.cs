9fc19e5041f070287a32e3da756790b1c58528df
Jovan <stamenkovicjovan15@gmail.com>
2026-04-13 20:14:51 +0200

    Dodato otvaranje google mapa na klik adrese

--- LumiumPortal.Web/Components/Pages/Clients/Details/ClientOverview.razor.cs
+++ LumiumPortal.Web/Components/Pages/Clients/Details/ClientOverview.razor.cs
@@ -1,9 +1,19 @@
 using Lumium.Application.Features.Clients.DTOs;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace LumiumPortal.Web.Components.Pages.Clients.Details;
 
 public partial class ClientOverview : ComponentBase
 {
+    [Inject] private IJSRuntime Js { get; set; } = null!;
+    
     [Parameter] [EditorRequired] public ClientDetailsDto Client { get; set; } = null!;
+    
+    private async Task OpenInMaps()
+    {
+        var query = Uri.EscapeDataString($"{Client.Address}, {Client.Country?.Name}");
+        var url = $"https://www.google.com/maps/search/?api=1&query={query}";
+        await Js.InvokeVoidAsync("open", url, "_blank");
+    }
 }
